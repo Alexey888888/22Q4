9030f96f5258070251a1a0105ceed0ee472cac8b
Alexey Borodako <brdkmail@gmail.com>
2023-02-19 22:50:02 +0300

    feat: add proPlayer

--- momentum/js/audio_play.js
+++ momentum/js/audio_play.js
@@ -4,6 +4,11 @@ const prev = document.querySelector(".play-prev");
 const next = document.querySelector(".play-next");
 const btnPlay = document.querySelector(".play");
 const playListContainer = document.querySelector(".play-list");
+//
+let current = document.querySelector(".time-track .current");
+
+//
+
 let playItem;
 let isPlay = false;
 let playNum = 0;
@@ -26,9 +31,12 @@ function playAudio() {
   if (playNum > 3) playNum = 0;
   if (playNum < 0) playNum = 3;
   audio.src = playList[playNum].src;
-  audio.currentTime = 0;
+  //audio.currentTime = 0;
   toggleBtn();
   playItem[playNum].classList.toggle("item-active");
+  document.querySelector(".current-track-name").innerHTML =
+    playItem[playNum].innerHTML;
+
   if (!isPlay) {
     isPlay = true;
     audio.play();
@@ -69,3 +77,80 @@ btnPlay.addEventListener("click", playAudio);
 
 next.addEventListener("click", playNext);
 prev.addEventListener("click", playPrev);
+
+/////////////////////////////////////////////////////
+
+const player = document.querySelector(".player");
+
+audio.addEventListener(
+  "loadeddata",
+  () => {
+    player.querySelector(".length").textContent = getTimeCodeFromNum(
+      audio.duration
+    );
+    audio.volume = 0.5;
+  },
+  false
+);
+
+//click on timeline to skip around
+const timeline = player.querySelector(".timeline");
+timeline.addEventListener(
+  "click",
+  (e) => {
+    const timelineWidth = window.getComputedStyle(timeline).width;
+    const timeToSeek = (e.offsetX / parseInt(timelineWidth)) * audio.duration;
+    audio.currentTime = timeToSeek;
+  },
+  false
+);
+
+//check audio percentage and update time accordingly
+setInterval(() => {
+  const progressBar = player.querySelector(".progress");
+  progressBar.style.width = (audio.currentTime / audio.duration) * 100 + "%";
+  current.textContent = getTimeCodeFromNum(audio.currentTime);
+}, 500);
+
+//turn 128 seconds into 2:08
+function getTimeCodeFromNum(num) {
+  let seconds = parseInt(num);
+  let minutes = parseInt(seconds / 60);
+  seconds -= minutes * 60;
+  const hours = parseInt(minutes / 60);
+  minutes -= hours * 60;
+
+  if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+  return `${String(hours).padStart(2, 0)}:${minutes}:${String(
+    seconds % 60
+  ).padStart(2, 0)}`;
+}
+
+////////////
+///////////
+//////////
+player.querySelector(".icono-volumeMedium").addEventListener("click", () => {
+  const volumeM = player.querySelector(".volume");
+  audio.muted = !audio.muted;
+  if (audio.muted) {
+    volumeM.classList.remove("icono-volumeMedium");
+    volumeM.classList.add("icono-volumeMute");
+  } else {
+    volumeM.classList.add("icono-volumeMedium");
+    volumeM.classList.remove("icono-volumeMute");
+  }
+});
+
+//click volume slider to change volume
+const volumeSlider = player.querySelector(".volume-slider");
+volumeSlider.addEventListener(
+  "click",
+  (e) => {
+    const sliderWidth = window.getComputedStyle(volumeSlider).width;
+    const newVolume = e.offsetX / parseInt(sliderWidth);
+    audio.volume = newVolume;
+    player.querySelector(".volume-percentage").style.width =
+      newVolume * 100 + "%";
+  },
+  false
+);
